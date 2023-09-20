f9b7d4fb1775d3e5e338016fe672f9c4e260a025
Sumant-Testbook <sumant.dusane@testbook.com>
2023-09-21 02:05:39 +0530

    reverted server

--- server/server.mjs
+++ server/server.mjs
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import "./loadEnvironment.mjs";
 import records from "./router/record.mjs";
-import getAllImages, { addAll } from "./db/listAllImages.mjs";
 
 
 const PORT = 9200;
@@ -12,8 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/record", records);
-// getAllImages();
-// addAll();
 
 // start the Express server
 app.listen(PORT, () => {
