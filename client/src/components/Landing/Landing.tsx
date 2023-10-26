eb28dafd62c52bccd3ea4eeeaa7339e7abea8bd4
Sumant-Testbook <109795886+Sumant-Testbook@users.noreply.github.com>
2023-10-26 17:57:26 +0530

    Database Reshift from Firebase to MongoDB (#9)
    * reshifted db from firebase to mongodb
    
    * replaced dependency with firebase to mongo

--- client/src/components/Landing/Landing.tsx
+++ client/src/components/Landing/Landing.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import "./landing.scss";
-import hero from './../../assets/images/hero.jpeg';
+import imagePlaceholder from './../../assets/images/img-placeholder.png';
 import ScrollToTop from '../ScrollToTop/ScrollToTop';
 import { Link } from 'react-router-dom';
 import { endpoint, strTransform } from '../../Utils/Utils';
@@ -20,11 +20,11 @@ export default function Landing() {
   function MapProjectCards(props: any) {
     return props.map((data: any, index: number) => {
       return (
-        <Link to={'/' + data.name} className="card" key={index}>
+        <Link to={'/' + data?.name} className="card" key={index}>
           <div className='card__overlay'>
-            <img className='card__img' src={data.photos.hero_img} alt="" />
-            <div className='card__title'>{strTransform(data.name)}</div>
-            <div className='card__subtitle'>{data.location}</div>
+            <img className='card__img' src={data?.photos?.hero_img ? data?.photos?.hero_img : imagePlaceholder} alt="" />
+            <div className='card__title'>{strTransform(data?.name)}</div>
+            <div className='card__subtitle'>{data?.location}</div>
             <div className='card__redirect'>Click to See Full View</div>
           </div>
         </Link>
