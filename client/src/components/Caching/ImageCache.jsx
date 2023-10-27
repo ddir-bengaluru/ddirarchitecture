3cb37a058c59873da3cfb1a30e62b31b491ab007
Sumant-Testbook <sumant.dusane@testbook.com>
2023-10-27 19:52:55 +0530

    successfully added caching functionality

--- client/src/components/Caching/ImageCache.jsx
+++ client/src/components/Caching/ImageCache.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { openDB } from "idb";
+import placeHolderImage from "./../../assets/images/img-placeholder.png";
 
 const DB_NAME = 'DDIRINDEXDB';
 const OBJECT_STORE_NAME = 'DDIRIMAGES';
@@ -16,82 +17,87 @@ async function cacheImage(url, blob) {
     const db = await openIndexDB();
     const tx = db.transaction(OBJECT_STORE_NAME, "readwrite");
     const store = tx.objectStore(OBJECT_STORE_NAME);
-    store.put(blob, url);
-    await tx.done();
+    store.add(blob, url);
+    await tx.done;
+    return URL.createObjectURL(blob);
 }
 
 async function getCachedImage(url) {
     const db = await openIndexDB();
-    const tx = db.transaction(OBJECT_STORE_NAME);
+    const tx = db.transaction(OBJECT_STORE_NAME, 'readonly');
     const store = tx.objectStore(OBJECT_STORE_NAME);
     return store.get(url);
 }
 
-export default function ImageCache({imageUrl, altText}) {
+export default function ImageCache({ imageUrl, altText }) {
     const [cachedImageUrl, setCachedImageUrl] = useState(null);
+    const [isLoading, setLoading] = useState(true);
+    const imgRef = useRef();
     useEffect(() => {
         async function loadImage() {
             const cachedImage = await getCachedImage(imageUrl);
             try {
-                if(cachedImage) {
-                    console.log('using cache');
-                    const blob = new Blob([cachedImage]);
-                    const objectUrl = URL.createObjectURL(blob);
-                    setCachedImageUrl(objectUrl);   
-                } else {
-                    console.log('not found in cache, using api ');
-                    const response = await fetch(imageUrl, {
-                        method: 'GET',
-                        mode: 'no-cors',
-                        headers: {
-                            'Access-Control-Allow-Origin': '*'
-                        }
-                    });
-                    const blob = await response.blob();
-                    await cacheImage(imageUrl, blob);
-                    const objectUrl = URL.createObjectURL(blob);
+                if (cachedImage) {
+                    const blob = cachedImage;
+                    const blobData = new Blob([blob]);
+                    const objectUrl = URL.createObjectURL(blobData);
                     setCachedImageUrl(objectUrl);
+                } else {
+                    let blob;
+                    const req = new Request(imageUrl);
+                    const res = await fetch(req)
+                    if(!res.ok) {
+                        console.error(res);
+                        return
+                    }
+                    blob = await res.blob();
+                    setCachedImageUrl(await cacheImage(imageUrl, blob));
+                    
+                    // const res = await fetch(req, {
+                    //     mode: 'cors',
+                    //     crossorigin: true,
+                    //     headers: {
+                    //         'Access-Control-Allow-Origin': '*'
+                    //     }
+                    // });
+                    // if (res.type === 'opaque') {
+                    //     blob = new Blob([await res.text()], {
+                    //         type: res.headers.get('content-type'),
+                    //     });
+                    //     const objectUrl = URL.createObjectURL(blob);
+                    //     setCachedImageUrl(objectUrl);
+                    // } else {
+                    //     blob = await res.blob();
+                    //     const objectUrl = URL.createObjectURL(blob);
+                    //     setCachedImageUrl(objectUrl);
+                    // }
+
                 }
             } catch (err) {
-                setCachedImageUrl('../../assets/images/img-placeholder.png');
+                console.error(err, cachedImageUrl);
+                if(!(err instanceof DOMException && err.name === 'AbortError') || !err.code === DOMException.ABORT_ERR) {
+                    setCachedImageUrl(placeHolderImage);
+                }
+            }
+            if (imgRef.current && imgRef.current.complete) {
+                setLoading(false);
             }
         }
         loadImage();
     }, [imageUrl]);
     return (
-        <img className='cached-image' src={cachedImageUrl} alt={altText} />
+        <>
+            <img
+                ref={imgRef} 
+                onLoad={() => setLoading(false)}
+                onError={(e) => {
+                    console.error("Image Loading Error: ", e);
+                    imgRef.current.style.display = 'none'
+                }}
+                loading="lazy"
+                src={isLoading ? placeHolderImage : cachedImageUrl} 
+                alt={altText}
+            />
+        </>
     )
 }
-
-
-// export const fetchAndCacheImage = async (imageUrl) => {
-//     const db = await openDB('DDIRDB', 1); // Open or create the database
-
-//     // Check if the image is already cached
-//     const cachedImage = await db.get('DDIRImages', imageUrl);
-
-//     if (!cachedImage) {
-//         // If not cached, fetch the image from the CDN
-//         const response = await fetch(imageUrl);
-//         const blob = await response.blob();
-
-//         // Store the image in IndexedDB
-//         const tx = db.transaction('DDIRImages', 'readwrite');
-//         tx.objectStore('DDIRImages').put(blob, imageUrl);
-//         await tx.complete;
-//     }
-// };
-
-// // Function to load the image from IndexedDB
-// export const loadImageFromIndexDB = async (imageUrl) => {
-//     const db = await idb.open('DDIRDB', 1); // Open the database
-//     const cachedImage = await db.get('DDIRImages', imageUrl);
-
-//     if (cachedImage) {
-//         // If the image is cached, use it
-//         const imageURL = URL.createObjectURL(cachedImage);
-//         console.log('Using cached image:', imageURL);
-//     } else {
-//         fetchAndCacheImage(imageUrl);
-//     }
-// };
