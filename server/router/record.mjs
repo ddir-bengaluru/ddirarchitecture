0c24cb695ca3d551473833d5cc3ed2d49746e95f
Sumant-Testbook <sumant.dusane@testbook.com>
2023-10-28 22:21:39 +0530

    pr fixes

--- server/router/record.mjs
+++ server/router/record.mjs
@@ -1,28 +1,12 @@
 import express from "express";
-// MongoDB
 import db from "../db/connection.mjs";
 
 const router = express.Router();
 
-<<<<<<< Updated upstream
 router.get("/", async (req, res) => {
   let collection = await db.collection("projects");
   let results = await collection.find({}).toArray();
   res.status(200).send(results);
-=======
-router.get('/', async(req, res) => {
-  const querySnapshot = await getDocs(collection(db, "projects"));
-  let results = [];
-  querySnapshot.forEach((doc) => {
-    results.push(doc.data());
-  });
-
-  if(!results) res.send('No Data').status(404);
-  else {
-    res.setHeader('Cache-Control', 'public, max-age=3600');
-    res.send(results).status(200);
-  }
->>>>>>> Stashed changes
 });
 
 router.get("/allClients", async (req, res) => {
@@ -38,7 +22,6 @@ router.get("/allClients", async (req, res) => {
   res.send(results).status(200);
 });
 
-<<<<<<< Updated upstream
 router.get("/:name", async (req, res) => {
   let collection = await db.collection("projects");
   let query = {name: req.params.name};
@@ -46,13 +29,6 @@ router.get("/:name", async (req, res) => {
 
   if (!result) res.sendStatus(404);
   else res.send(result).status(200);
-=======
-  if (!result) res.send({}).status(404);
-  else {
-    res.setHeader('Cache-Control', 'public, max-age=3600');
-    res.send(result).status(200);
-  }
->>>>>>> Stashed changes
 });
 
 router.get("/category/:name", async (req, res) => {
@@ -94,159 +70,7 @@ router.get("/search/:name", async (req, res) =>{
   results.push(...projectResults, ...artResults);
 
   if (!results.length) res.send([]).status(404);
-<<<<<<< Updated upstream
   else res.send(results).status(200)
 });
 
-// Add To MongoDocument 
-=======
-  else {
-    res.setHeader('Cache-Control', 'public, max-age=3600');
-    res.send(results).status(200)
-  };
-});
-
-router.get("/art/:name", async(req, res) => {
-  const projectRef = collection(db, "art");
-  let q = query(projectRef, where("name","==",req.params.name));
-  const querySnapshot = await getDocs(q);
-  let result = {};
-  querySnapshot.forEach(doc => {
-    result = doc.data();
-  })
-
-  if(!result) res.send({}).status(404);
-  else {
-    res.setHeader('Cache-Control', 'public, max-age=3600');
-    res.send(result).status(200)
-  };
-});
-
 export default router;
-
-
-
-
-
-
-
-
-// router.get("/", async (req, res) => {
-//   let collection = await db.collection("projects");
-//   let results = await collection.find({}).toArray();
-//   res.send(results).status(200);
-// });
-
-// router.get("/:name", async (req, res) => {
-//   let collection = await db.collection("projects");
-//   let query = {name: req.params.name};
-//   let result = await collection.findOne(query);
-
-//   if (!result) res.sendStatus(404);
-//   else res.send(result).status(200);
-// });
-
-// router.get("/category/:name", async (req, res) => {
-//   let collection = await db.collection("projects");
-//   let query = {category: req.params.name};
-//   let result = await collection.find(query).toArray();
-
-//   if(!result) res.send("No Data").status(404);
-//   else res.send(result).status(200);
-// });
-
->>>>>>> Stashed changes
-// router.post("/", async (req, res) => {
-//   let newDocument = {
-//     name: req.body.name,
-//     location: req.body.location,
-//   };
-//   let collection = await db.collection("projects");
-//   let result = await collection.insertOne(newDocument);
-//   res.send(result).status(204);
-// });
-
-
-// Get DB MongoDocument
-// router.get('/', async(req, res) => {
-//   let snapshot = await app.get();
-//   res.send(snapshot).status(200);
-// })
-
-export default router;
-
-
-
-
-
-
-
-
-// Firebase
-// import { collection, getDocs, query, where } from "firebase/firestore"; 
-// router.get('/', async(req, res) => {
-//   const querySnapshot = await getDocs(collection(db, "projects"));
-//   let results = [];
-//   querySnapshot.forEach((doc) => {
-//     results.push(doc.data());
-//   });
-
-//   if(!results) res.send('No Data').status(404);
-//   else res.send(results).status(200);
-// });
-
-// router.get('/:name', async(req, res) => {
-//   const projectRef = collection(db, "projects");
-//   let q = query(projectRef, where("name","==",req.params.name));
-//   const querySnapshot = await getDocs(q);
-//   let result = {};
-//   querySnapshot.forEach(doc => {
-//     result = doc.data();
-//   });
-
-//   if (!result) res.send({}).status(404);
-//   else res.send(result).status(200);
-// });
-
-// router.get("/category/:name", async (req, res) => {
-//   const projectRef = collection(db, "projects");
-//   let q = query(projectRef, where("category","==",req.params.name));
-//   const querySnapshot = await getDocs(q);
-//   let results = [];
-//   querySnapshot.forEach(doc => {
-//     results.push(doc.data());
-//   });
-
-//   if (!results?.length) res.send([]).status(404);
-//   else res.send(results).status(200);
-// });
-
-// router.get("/search/:name", async (req, res) =>{
-//   const projectRef = collection(db, "projects");
-//   const querySnapshot = await getDocs(projectRef);
-//   let results = [];
-//   querySnapshot.forEach(doc => {
-//     let nameKey = doc.data().name.toLowerCase();
-//     let clientNameKey = doc.data().client_name.toLowerCase();
-//     let searchQuery = req.params.name.toLowerCase();
-//     if(nameKey.includes(searchQuery) || clientNameKey.includes(searchQuery)) {
-//       results.push(doc.data());
-//     }
-//   })
-
-//   if (!results.length) res.send([]).status(404);
-//   else res.send(results).status(404);
-// });
-
-// router.get("/art/:name", async(req, res) => {
-//   const projectRef = collection(db, "art");
-//   let q = query(projectRef, where("name","==",req.params.name));
-//   const querySnapshot = await getDocs(q);
-//   let result = {};
-//   querySnapshot.forEach(doc => {
-//     result = doc.data();
-//   })
-
-//   if(!result) res.send({}).status(404);
-//   else res.send(result).status(200);
-// });
